1ce20fc12d767d960b4677fade72f9c2a1633b44
Tony Deng <wolf.deng@gmail.com>
2022-10-15 18:51:23 +0800

    :sparkles: feat(day01): 添加print的脚本
    添加print的脚本
    
    Signed-off-by: Tony Deng <wolf.deng@gmail.com>

---
+++ Day01-15/code/Day01/print_name.py
@@ -0,0 +1,19 @@
+# 这是注释
+
+'''
+这是注释
+'''
+
+
+print("Hello World")
+
+name = '小明'
+age = 6
+height = 103.2
+
+# 输出小明今年6岁，身高103.2厘米
+
+
+#print(name+'今年'+str(age) + '岁，身高’+str(height)+'厘米')
+
+print(name+'今年'+str(age)+'岁，身高'+str(height)+'厘米')
---
+++ Day01-15/code/Day01/print_name_b.py
@@ -0,0 +1,18 @@
+name = '小明'
+age = 6
+height = 103.2
+
+print(name + '今年' + str(age) + '岁，身高'+str(height)+'厘米')
+
+# 将一个人名存储成一个变量
+
+name  = "TonY Deng"
+print(name)
+print(name.lower())
+print(name.upper())
+print(name.title())
+print(name.capitalize())
+
+
+name = str(input("请输入姓名："))
+print(name.upper())
